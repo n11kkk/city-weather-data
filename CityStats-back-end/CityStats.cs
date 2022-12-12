d08f9bbd76db8472fb95d8328418a27483fcf230
Nikilesh <nxsambamurthy@shockers.wichita.edu>
2022-12-11 19:54:41 -0600

    Added boilerplate for backend and api for wichita weather data.

---
+++ CityStats-back-end/CityStats.cs
@@ -0,0 +1,50 @@
+﻿namespace CityStats_back_end
+{
+
+
+    public partial class CityStats
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+        public float generationtime_ms { get; set; }
+        public int utc_offset_seconds { get; set; }
+        public string timezone { get; set; }
+        public string timezome_abbreviation { get; set; }
+        public float elevation { get; set; }
+        public DailyUnits daily_units { get; set; }
+        public Daily daily { get; set; }
+    }
+
+    public partial class Daily
+    {
+        public List<DateTimeOffset> time { get; set; }
+        public List<int> weathercode { get; set; }
+        public List<float> temperature_2m_max { get; set; }
+        public List<float> temperature_2m_min { get; set; }
+        public List<string> sunrise { get; set; }
+        public List<string> sunset { get; set; }
+        public List<float> windspeed_10m_max { get; set; }
+    }
+
+    public partial class DailyUnits
+    {
+        public string time { get; set; }
+        public string weathercode { get; set; }
+        public string temperature_2m_max { get; set; }
+        public string temperature_2m_min { get; set; }
+        public string sunrise { get; set; }
+        public string sunset { get; set; }
+        public string windspeed_10m_max { get; set; }
+    }
+
+
+    public partial class JokeModel
+    {
+        public long Id { get; set; }
+        public string? Type { get; set; }
+        public string? Setup { get; set; }
+        public string? Punchline { get; set; }
+    }
+}
+
+
---
+++ CityStats-back-end/Controllers/CityStatsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace CityStats_back_end.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CityStatsController : ControllerBase
+    {
+        HttpClient _client;
+        readonly string WeatherAPI = "https://api.open-meteo.com/v1/forecast?latitude=37.69&longitude=-97.34&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,windspeed_10m_max&timezone=America%2FChicago";
+        //readonly string JokeAPI = "https://official-joke-api.appspot.com/random_joke";
+        public CityStatsController()
+        {
+            _client = new HttpClient();
+
+        }
+
+
+
+        [HttpGet(Name = "GetCityStats")]
+        public async Task<CityStats> Get()
+        {
+            HttpResponseMessage res = await _client.GetAsync(WeatherAPI);
+            System.Diagnostics.Debug.WriteLine(await res.Content.ReadAsStringAsync());
+            string stringres = await res.Content.ReadAsStringAsync();
+            //CityStats jsonRes = await res.Content.ReadFromJsonAsync<CityStats>();
+            CityStats? jsonRes = JsonSerializer.Deserialize<CityStats>(stringres);
+
+
+            System.Diagnostics.Debug.WriteLine("___________________JSON_______________ \n",jsonRes);
+
+            DailyUnits du = new DailyUnits
+            {
+                time = jsonRes.daily_units.time,
+                weathercode = jsonRes.daily_units.weathercode,
+                temperature_2m_max = jsonRes.daily_units.temperature_2m_max,
+                temperature_2m_min = jsonRes.daily_units.temperature_2m_min,
+                sunrise = jsonRes.daily_units.sunrise,
+                sunset = jsonRes.daily_units.sunset,
+                windspeed_10m_max = jsonRes.daily_units.windspeed_10m_max
+            };
+            Daily dd = new Daily
+            {
+                time = jsonRes.daily.time,
+                weathercode = jsonRes.daily.weathercode,
+                temperature_2m_max = jsonRes.daily.temperature_2m_max,
+                temperature_2m_min = jsonRes.daily.temperature_2m_min,
+                sunrise = jsonRes.daily.sunrise,
+                sunset = jsonRes.daily.sunset,
+                windspeed_10m_max = jsonRes.daily.windspeed_10m_max
+            };
+
+
+            return new CityStats
+            {
+                latitude = jsonRes.latitude,
+                longitude = jsonRes.longitude,
+                generationtime_ms = jsonRes.generationtime_ms,
+                utc_offset_seconds = jsonRes.utc_offset_seconds,
+                timezone = jsonRes.timezone,
+                timezome_abbreviation = jsonRes.timezome_abbreviation,
+                elevation = jsonRes.elevation,
+                daily_units = du,
+                daily = dd
+                
+            };
+        }
+
+
+
+    }
+}
